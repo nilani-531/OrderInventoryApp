646a344fdb6c8cc9375ed639303ac339fe23a31c
nilani-531 <nilani@gmail.com>
2026-04-13 14:33:00 +0530

    intializeProjectCommited

---
+++ src/test/java/com/sprintProject/OrderInventoryApplication/OrderInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintProject.OrderInventoryApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
