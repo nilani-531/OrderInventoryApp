d272a0422af45d626fee6e223f60f77454ee7b8e
Pooja <poojalakshmip2004@gmail.com>
2026-04-15 14:42:41 +0530

    Repository : Stores Created

--- src/main/java/com/sprintProject/OrderInventoryApplication/EntityClasses/Stores.java
+++ src/main/java/com/sprintProject/OrderInventoryApplication/EntityClasses/Stores.java
@@ -1,129 +1,93 @@
-package com.yourpackage.stores.entity;
+package com.sprintProject.OrderInventoryApplication.EntityClasses;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-
-import java.math.BigDecimal;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "stores")
-public class Store {
+public class Stores {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer storeId;
-
-    @NotBlank
-    private String storeName;
-
-    private String webAddress;
-
-    private String physicalAddress;
-
-    private BigDecimal latitude;
-
-    private BigDecimal longitude;
-
+    private int store_id;
+    @NotNull
+    private String store_name;
+    private String web_address;
+    private String physical_address;
+    private double latitude;
+    private double longitude;
     @Lob
     private byte[] logo;
+    private String logo_mime_type;
+    private String logo_filename;
+    private String logo_charset;
+    private LocalDate logo_last_updated;
 
-    private String logoMimeType;
-
-    private String logoFilename;
-
-    private String logoCharset;
-
-    private LocalDate logoLastUpdated;
-
-    public Store() {
+    public int getStoreId() {
+        return store_id;
     }
-
-    public Integer getStoreId() {
-        return storeId;
+    public void setStoreId(int store_id) {
+        this.store_id = store_id;
     }
-
-    public void setStoreId(Integer storeId) {
-        this.storeId = storeId;
-    }
-
     public String getStoreName() {
-        return storeName;
+        return store_name;
     }
-
-    public void setStoreName(String storeName) {
-        this.storeName = storeName;
+    public void setStoreName(String store_name) {
+        this.store_name = store_name;
     }
-
     public String getWebAddress() {
-        return webAddress;
+        return web_address;
     }
-
-    public void setWebAddress(String webAddress) {
-        this.webAddress = webAddress;
+    public void setWebAddress(String web_address) {
+        this.web_address = web_address;
     }
-
     public String getPhysicalAddress() {
-        return physicalAddress;
+        return physical_address;
     }
-
-    public void setPhysicalAddress(String physicalAddress) {
-        this.physicalAddress = physicalAddress;
+    public void setPhysicalAddress(String physical_address) {
+        this.physical_address = physical_address;
     }
-
-    public BigDecimal getLatitude() {
+    public double getLatitude() {
         return latitude;
     }
-
-    public void setLatitude(BigDecimal latitude) {
+    public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
-
-    public BigDecimal getLongitude() {
+    public double getLongitude() {
         return longitude;
     }
-
-    public void setLongitude(BigDecimal longitude) {
+    public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
-
     public byte[] getLogo() {
         return logo;
     }
-
     public void setLogo(byte[] logo) {
         this.logo = logo;
     }
-
     public String getLogoMimeType() {
-        return logoMimeType;
+        return logo_mime_type;
     }
-
-    public void setLogoMimeType(String logoMimeType) {
-        this.logoMimeType = logoMimeType;
+    public void setLogoMimeType(String logo_mime_type) {
+        this.logo_mime_type = logo_mime_type;
     }
-
     public String getLogoFilename() {
-        return logoFilename;
+        return logo_filename;
     }
-
-    public void setLogoFilename(String logoFilename) {
-        this.logoFilename = logoFilename;
+    public void setLogoFilename(String logo_filename) {
+        this.logo_filename = logo_filename;
     }
-
     public String getLogoCharset() {
-        return logoCharset;
+        return logo_charset;
     }
-
-    public void setLogoCharset(String logoCharset) {
-        this.logoCharset = logoCharset;
+    public void setLogoCharset(String logo_charset) {
+        this.logo_charset = logo_charset;
     }
-
     public LocalDate getLogoLastUpdated() {
-        return logoLastUpdated;
+        return logo_last_updated;
     }
-
-    public void setLogoLastUpdated(LocalDate logoLastUpdated) {
-        this.logoLastUpdated = logoLastUpdated;
+    public void setLogoLastUpdated(LocalDate logo_last_updated) {
+        this.logo_last_updated = logo_last_updated;
     }
 }
