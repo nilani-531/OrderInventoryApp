94b370d1a4bd44a77d537b1071094e3eb07009c1
karthi-kn04 <karthikn200412@gmail.com>
2026-04-15 13:27:53 +0530

    Added Inventory & Products Entity files

--- src/main/java/com/sprintProject/OrderInventoryApplication/EntityClasses/Inventory.java
+++ src/main/java/com/sprintProject/OrderInventoryApplication/EntityClasses/Inventory.java
@@ -5,9 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
+@Table(name = "inventory")
 public class Inventory {
 	
 	@Id
--- src/main/java/com/sprintProject/OrderInventoryApplication/EntityClasses/Products.java
+++ src/main/java/com/sprintProject/OrderInventoryApplication/EntityClasses/Products.java
@@ -6,9 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
+@Table(name = "products")
 public class Products {
 	
 	@Id
