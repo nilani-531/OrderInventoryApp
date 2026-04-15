a95e5ee5b04d912f644fb1b799db7e2306191bd5
NILANI S <128598215+nilani-531@users.noreply.github.com>
2026-04-15 15:42:25 +0530

    Delete src/main/java/com/sprintProject/OrderInventoryApplication/RepositoryInterfaces directory

--- src/main/java/com/sprintProject/OrderInventoryApplication/RepositoryInterfaces/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sprintProject.OrderInventoryApplication.RepositoryInterfaces;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sprintProject.OrderInventoryApplication.EntityClasses.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
-
-}
