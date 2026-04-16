21eafc93463337604158723e9130a75d94449471
karthi-kn04 <karthikn200412@gmail.com>
2026-04-16 10:51:11 +0530

    Added Product & Inventory - serviceLayer

--- src/main/java/com/sprintProject/OrderInventoryApplication/RepositoryLayer/InventoryRepository.java
+++ src/main/java/com/sprintProject/OrderInventoryApplication/RepositoryLayer/InventoryRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sprintProject.OrderInventoryApplication.EntityClasses.Inventory;
 
+import java.util.List;
+
 public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
+
+    List<Inventory> findByStores_StoreId(int storeId);
+
+    List<Inventory> findByProducts_ProductId(int productId);
 
 }
---
+++ src/main/java/com/sprintProject/OrderInventoryApplication/ServiceLayer/InventoryServiceInterface.java
@@ -0,0 +1,24 @@
+package com.sprintProject.OrderInventoryApplication.ServiceLayer;
+
+import java.util.List;
+
+import com.sprintProject.OrderInventoryApplication.EntityClasses.Inventory;
+
+public interface InventoryServiceInterface {
+	
+
+	    List<Inventory> getAllInventory();
+
+	    Inventory getInventoryById(int inventoryId);
+
+	    Inventory createInventory(Inventory inventory);
+
+	    Inventory updateInventory(int inventoryId, Inventory inventory);
+
+	    void deleteInventory(int inventoryId);
+
+	    List<Inventory> getInventoryByStoreId(int storeId);
+
+	    List<Inventory> getInventoryByProductId(int productId);
+	
+}
