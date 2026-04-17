ca283a7bbe816172103f651c2395ae0585164965
Pooja <poojalakshmip2004@gmail.com>
2026-04-17 08:45:53 +0530

    Stores : Service and CustomExceptions

--- src/main/java/com/sprintProject/OrderInventoryApplication/CustomExceptions/InvalidDataException.java
+++ src/main/java/com/sprintProject/OrderInventoryApplication/CustomExceptions/InvalidDataException.java
@@ -1,4 +1,7 @@
 package com.sprintProject.OrderInventoryApplication.CustomExceptions;
 
-public class InvalidDataException {
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException(String message) {
+        super(message);
+    }
 }
