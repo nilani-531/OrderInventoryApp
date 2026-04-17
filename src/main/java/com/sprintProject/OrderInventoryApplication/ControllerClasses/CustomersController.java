1992c07cce62d38125dda9fa437b7ee87eb09348
Abikayal <abinayakayal2004@gmail.com>
2026-04-17 14:44:06 +0530

    day23

--- src/main/java/com/sprintProject/OrderInventoryApplication/ControllerClasses/CustomersController.java
+++ src/main/java/com/sprintProject/OrderInventoryApplication/ControllerClasses/CustomersController.java
@@ -7,6 +7,7 @@ package com.sprintProject.OrderInventoryApplication.ControllerClasses;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,7 @@ import com.sprintProject.OrderInventoryApplication.EntityClasses.Shipments;
 import com.sprintProject.OrderInventoryApplication.ServiceLayer.CustomersService;
 import com.sprintProject.OrderInventoryApplication.dto.requestDto.CustomersRequestDto;
 import com.sprintProject.OrderInventoryApplication.dto.responseDto.CustomersResponseDto;
+import com.sprintProject.OrderInventoryApplication.dto.responseDto.ResponseStructure;
 
 @RestController
 @RequestMapping("/api/customers")
