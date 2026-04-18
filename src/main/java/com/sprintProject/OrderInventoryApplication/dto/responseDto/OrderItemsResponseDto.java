e35297656a91636ca011a5dc19d8ffa86027e195
Abikayal <146316902+Abikayal@users.noreply.github.com>
2026-04-18 14:36:21 +0530

    Refactor OrderItemsResponseDto for cleaner code (#39)

--- src/main/java/com/sprintProject/OrderInventoryApplication/dto/responseDto/OrderItemsResponseDto.java
+++ src/main/java/com/sprintProject/OrderInventoryApplication/dto/responseDto/OrderItemsResponseDto.java
@@ -10,74 +10,35 @@ public class OrderItemsResponseDto {
 
     private int quantity;
     private double unitPrice;
+	public int getLineItemId() {
+		return lineItemId;
+	}
+	public void setLineItemId(int lineItemId) {
+		this.lineItemId = lineItemId;
+	}
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
 
-    public int getLineItemId() {
-        return lineItemId;
-    }
-
-    public void setLineItemId(int lineItemId) {
-        this.lineItemId = lineItemId;
-    }
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-
-    public int getProductId() {
-        return productId;
-    }
-
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-public class OrderItemsResponseDto {
-	 private int lineItemId;
-     private double unitPrice;
-	 public int getLineItemId() {
-		 return lineItemId;
-	 }
-	 private int quantity;
-
-
-    public double getUnitPrice() {
-        return unitPrice;
-    }
-
-
-    public void setUnitPrice(double unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-}
-
- public void setQuantity(int quantity) {
-	     this.quantity = quantity;
-	 }
-	 public void setLineItemId(int lineItemId) {
-		 this.lineItemId = lineItemId;
-	 }
-	 public double getUnitPrice() {
-		 return unitPrice;
-	 }
-	 public void setUnitPrice(double unitPrice) {
-		 this.unitPrice = unitPrice;
-	 }
-	 @Override
-	 public String toString() {
-		return "OrderItemsResponseDto [lineItemId=" + lineItemId +  ", unitPrice=" + unitPrice + "]";
-	 }
-	 
-}
-
+ }
