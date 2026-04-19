cdd9068ee5dd5ff78301a132a18ae31cc97abbc9
karthi-kn04 <karthikn200412@gmail.com>
2026-04-19 16:25:04 +0530

    added comments

--- src/main/java/com/sprintProject/OrderInventoryApplication/RepositoryLayer/InventoryRepository.java
+++ src/main/java/com/sprintProject/OrderInventoryApplication/RepositoryLayer/InventoryRepository.java
@@ -9,9 +9,11 @@ import java.util.List;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
 
+	// Find inventory by store ID
 	@Query("SELECT i FROM Inventory i WHERE i.stores.storeId = :storeId")
     List<Inventory> findByStoresStoreId(int storeId);
 
+	// Find inventory by product ID
 	@Query("SELECT i FROM Inventory i WHERE i.products.productId = :productId")
     List<Inventory> findByProductsProductId(int productId);
 
