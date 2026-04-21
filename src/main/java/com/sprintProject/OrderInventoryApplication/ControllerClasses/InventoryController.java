b68a5b5424f5718d951d6e578ee65438aab389ea
karthi-kn04 <karthikn200412@gmail.com>
2026-04-21 08:13:20 +0530

    @crossorigin

--- src/main/java/com/sprintProject/OrderInventoryApplication/ControllerClasses/InventoryController.java
+++ src/main/java/com/sprintProject/OrderInventoryApplication/ControllerClasses/InventoryController.java
@@ -3,6 +3,7 @@ package com.sprintProject.OrderInventoryApplication.ControllerClasses;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.sprintProject.OrderInventoryApplication.ServiceLayer.InventoryService
 
 @RestController
 @RequestMapping("/inventory")
+@CrossOrigin(origins = "*")
 public class InventoryController {
 
 	@Autowired
--- src/main/java/com/sprintProject/OrderInventoryApplication/ControllerClasses/ProductsController.java
+++ src/main/java/com/sprintProject/OrderInventoryApplication/ControllerClasses/ProductsController.java
@@ -3,6 +3,7 @@ package com.sprintProject.OrderInventoryApplication.ControllerClasses;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.sprintProject.OrderInventoryApplication.ServiceLayer.ProductsService;
 
 @RestController
 @RequestMapping("/products")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class ProductsController {
 
 	@Autowired
