1ee94ead26e6ffe6cc1187a572c557738f4fd02a
FrontendCode <nilani@email.com>
2026-04-28 09:05:48 +0530

    fb

--- src/main/java/com/sprintProject/OrderInventoryApplication/OrderInventoryApplication.java
+++ src/main/java/com/sprintProject/OrderInventoryApplication/OrderInventoryApplication.java
@@ -1,4 +1,4 @@
-package com.sprintProject.orderinventoryapplication;
+package com.sprintProject.OrderInventoryApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
